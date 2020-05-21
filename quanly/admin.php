efe4ddcaed06309d22b3bcb2cbdf5e3fd867c94c
nhung9598 <phinhung171198@gmail.com>
2020-05-22 00:27:13 +0700

    del

--- quanly/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(!isset($_SESSION["loged"])){
-		header("location:index.php");
-		setcookie("error", "Bạn chưa đăng nhập!", time()+1, "/","", 0);
-	}
-	else
-		echo "phần mềm quản lý phòng khám";
- 
-?>
